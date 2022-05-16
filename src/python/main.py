a5f62d9481131718050ab4c4e25a7ce2e6c5bca9
Reece Harris <reeceharris@email.com>
2022-05-16 16:13:38 +0100

    update

--- src/python/main.py
+++ src/python/main.py
@@ -1,4 +1,3 @@
-from ast import Try
 import time
 import hashlib
 
@@ -31,43 +30,66 @@ class prng:
         self.i = (self.i + 1) % 624
         return y
 
-def noiseMap(sd, se):
+def noiseMap(seed, size):
     nm = []
-    rand = prng(sd)
-    for i in range(se[1]):
+    rand = prng(seed)
+    for i in range(size[1]):
         nm.append([])
-        for j in range(se[0]):
+        for j in range(size[0]):
             nm[i].append((int(str(rand.rand())[1]) / 9))
     return nm
 
-# using interpolation make a smoother map and make it more readable for the user
-def interpolate(map, size):
-    for x in range(len(map)):
-        for y in range(len(map[x])):
-            if x == 0:
-                map[x][y] = map[x][y]
-            elif x == len(map) - 1:
-                map[x][y] = map[x][y]
-            else:
-                map[x][y] = (map[x - 1][y] + map[x + 1][y]) / 2
-    for x in range(len(map)):
-        for y in range(len(map[x])):
-            if y == 0:
-                map[x][y] = map[x][y]
-            elif y == len(map[x]) - 1:
-                map[x][y] = map[x][y]
-            else:
-                map[x][y] = (map[x][y - 1] + map[x][y + 1]) / 2
+def interpolate(map, intensity):
+    for i in range(intensity):
+        for x in range(len(map)):
+            for y in range(len(map[x])):
+                if x == 0:
+                    map[x][y] = map[x][y]
+                elif x == len(map) - 1:
+                    map[x][y] = map[x][y]
+                else:
+                    map[x][y] = (map[x - 1][y] + map[x + 1][y]) / 2
+        for x in range(len(map)):
+            for y in range(len(map[x])):
+                if y == 0:
+                    map[x][y] = map[x][y]
+                elif y == len(map[x]) - 1:
+                    map[x][y] = map[x][y]
+                else:
+                    map[x][y] = (map[x][y - 1] + map[x][y + 1]) / 2
     return map
 
-# take a noise map and round all the numbers to 1 decimal place
 def roundMap(map):
     for x in range(len(map)):
         for y in range(len(map[x])):
             map[x][y] = round(map[x][y], 1)
     return map
 
-def display(map, seed, coords, size, ts):
+def asciiMap(map):
+    switcher = {
+        '0.0': '-',
+        '0.1': '~',
+        '0.2': '=',
+        '0.3': '#',
+        '0.4': '░',
+        '0.5': '▒',
+        '0.6': '▓' 
+    }
+    for x in range(len(map)):
+        for y in range(len(map[x])):
+            map[x][y] = switcher.get(str(map[x][y]), ' ')
+    return map
+
+def display(map, seed, coords, size, ts, intensity):
+        loopIndex = 0
+        print(f'┏{"━" * (size[0] + 2)}┓')
+        for x in map:
+            loopIndex += 1
+            print("┃ ", end="")
+            for i in x:
+                print(str(i), end="")
+            print(f" ┃")
+        print(f'┗{"━" * (size[0] + 2)}┛')
 
         dcoords = f'X: {coords[0]}, Y: {coords[1]}'
         dtime = f'{ts} /ms'
@@ -77,28 +99,19 @@ def display(map, seed, coords, size, ts):
 
         print(f'''┏{"━" * (len(hashlib.sha256(str(map).encode('utf-8')).hexdigest()) + 13)}┓\n┃ // Settings {" " * (len(dchecksum))}┃\n┃ Seed     : {dseed[:len(dchecksum) - 2]}{'..' if (len(str(dseed)) > len(dchecksum)) else '' }{" " * ((len(dchecksum) ) - len(dseed))} ┃\n┃ Coords   : {dcoords} {" " * (len(dchecksum) - len(dcoords))}┃\n┃{" " * (len(dchecksum) + 13)}┃\n┃ // Algorithm Info {" " * (len(dchecksum) - 6)}┃\n┃ Checksum : {dchecksum} ┃\n┃ Time     : {dtime} {" " * (len(dchecksum) - len(dtime))}┃\n┃ Size     : {dsize} {" " * (len(dchecksum) - len(dsize))}┃\n┗{"━" * (len(dchecksum) + 13)}┛''')
 
-        loopIndex = 0
-        print(f'┏{"━" * (size[0] + 2)}┓')
-        for x in map:
-            loopIndex += 1
-            print("┃ ", end="")
-            for i in x:
-                print(str(i).replace('0.0', '-').replace('0.1', '~').replace('0.2', '=').replace('0.3', '#').replace('0.4', '░').replace('0.5', '▒').replace('0.6', '▓').replace(str(i), ' ') , end="")
-            print(f" ┃")
-
-        print(f'┗{"━" * (size[0] + 2)}┛')
-
 if __name__ == "__main__":
     # Initialize the parameters
     seed = 101012 #random.randint(0, 1000000)
     coords = (0, 0)
     size = (80, 40)
+    intensity = 3
 
     ts = time.time() # Start the timer
 
     map = noiseMap(seed, size)
-    smooth = interpolate(map, size)
+    smooth = interpolate(map, intensity)
     rounded = roundMap(map)
+    ascii = asciiMap(rounded)
 
     ts = round((time.time() - ts) * 1000, 2) # Stop the timer
-    display(rounded, seed, coords, size, ts)
+    display(ascii, seed, coords, size, ts, intensity)
